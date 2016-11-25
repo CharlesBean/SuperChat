df1e3d5f4444abfb814af1630fa800f5ec201eb3
Conner Bean <beanconn@msu.edu>
2016-11-25 18:56:03 -0500

    handler for WSS

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,17 @@ use mio::deprecated::Handler;
 struct WebSocketServer;
 impl Handler for WebSocketServer{
   type Timeout = usize;
-  type message = ();
+  type Message = ();
 }
 
 
 fn main() {
+    let mut event_loop = EventLoop::new().unwrap();
+
+    //creates new instance of Handler
+    let mut handler = WebSocketServer;
+    event_loop.run(&mut handler).unwrap();
+
+    
     println!("Hello, world!");
 }
